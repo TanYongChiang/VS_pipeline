9030732f3b6c16682a8c24977aeb4afab3c1845b
Alex Tan Yong Chiang <73526602+TanYongChiang@users.noreply.github.com>
2021-06-07 05:13:52 +0800

    (AD ONLY) prepare.sh generate best struc
    Added function to generate best function in autodock only (complex and ligand alone)

--- Modules/vs_pipeline.sh
+++ Modules/vs_pipeline.sh
@@ -2,30 +2,51 @@
 set -e
 set +a
 
+    #region add trap
 log() { printf '%s\n' "$*"; }
 error() { log "ERROR: $*" >&2; }
 fatal() { error "$@"; exit 1; }
 
+# appends a command to a trap
+#
+# - 1st arg:  code to add
+# - remaining args:  names of traps to modify
+#
 trap_add() {
     trap_add_cmd=$1; shift || fatal "${FUNCNAME} usage error"
     for trap_add_name in "$@"; do
         trap -- "$(
+            # helper fn to get existing trap command from output
+            # of trap -p
             extract_trap_cmd() { printf '%s\n' "$3"; }
+            # print existing trap command with newline
             eval "extract_trap_cmd $(trap -p "${trap_add_name}")"
+            # print the new trap command
             printf '%s\n' "${trap_add_cmd}"
         )" "${trap_add_name}" \
             || fatal "unable to add to trap ${trap_add_name}"
     done
 }
+# set the trace attribute for the above function.  this is
+# required to modify DEBUG or RETURN traps because functions don't
+# inherit them unless the trace attribute is set
 declare -f -t trap_add
+    # endregion add trap
+        # usage: trap_add 'command' EXIT
 
+# region others 
+# upon exit run this function
 function remove_tmp_file_parallel {
+    # remove temp_file for suppress warning only
     rm ${temp_file}
 }
 trap remove_tmp_file_parallel EXIT
 
+# create temporary file to suppress warning in parallel -j0 only
 temp_file=$(mktemp)
 
+# Set prot_ext
+## protein files pdbqt gpf dpf or pdbqt txt
 if [[ $ALGO == "AD" ]]
 then
     prot_ext=".pdbqt .gpf .dpf"
@@ -33,6 +54,9 @@ elif [[ $ALGO == "VINA" ]]
 then
     prot_ext=".pdbqt .txt"
 fi
+# endregion
+
+# region functions 
 function check_exist () 
 {
     if [ -e "$1" ]; then
@@ -45,7 +69,12 @@ function check_exist ()
 
 function vina_check_cpu () 
 {
+    # check if number of cpu has been declared in config.txt
+    # if present, then delete the cpu no. declaration
+    # 1- receptor name
+    # 2- protein_dir
     cpu_present=$(cat $2/$1.txt | awk '/(cpu =|cpu=)/ {print}')
+    # if output is null
     if [ -z $cpu_present ]
     then 
         :
@@ -56,12 +85,14 @@ function vina_check_cpu ()
 
 function prepareGPF () 
 {
+# dir, receptor name, lig name, MGL_dir
     cd $1
     $4/bin/pythonsh $4/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_gpf4.py -l $3.pdbqt -r $2.pdbqt -i $2.gpf -o grid.gpf > /dev/null 2>&1
 }
 
 function prepareDPF ()
 {
+# dir, receptor name, lig name, MGL_dir
     cd $1
     if [[ $(grep -w "do_local_only" $2.dpf | wc -l) -ge 1 ]]
     then
@@ -84,24 +115,35 @@ function prepareDPF ()
 
 function autogrid () 
 {
+# receptor name, lig name, working dir
     cd $3/docking/$1/$2/
     autogrid4 -p grid.gpf -l grid.glg > /dev/null 2>&1
 }
 
 function autodock () 
 {
+# receptor name, lig name, working dir
     cd $3/docking/$1/$2/
     autodock4 -p dock.dpf -l $1__$2.dlg > /dev/null 2>&1
 }
 
 function ad_vina ()
 {
+    #---------------------------
+    # 1- receptor name, 
+    # 2- lig name, 
+    # 3- working dir, 
+    #---------------------------
     cd $3/docking/$1/$2/
     vina --receptor $1.pdbqt --ligand $2.pdbqt --config $1.txt --out out.pdbqt --log log.pdbqt > /dev/null 2>&1
 }
 
 function vina_summaries ()
 {
+    # 1- receptor name, 
+    # 2- lig name, 
+    # 3- working dir,
+    # 4- result_dir
     cd $3/docking/$1/$2/
     result_byline=$(awk 'BEGIN {FS=" "; OFS=","}; /REMARK VINA RESULT/ {print "'$1'","'$2'",$4}' out.pdbqt | head -n 1)
     printf "$result_byline" | tr -d [:space:] >> $4/summary_virtualscreening.csv
@@ -110,8 +152,12 @@ function vina_summaries ()
 
 function vs_genenergymatrix ()
 {
+# 1- working_dir
+# 2- result_dir
+# 3- ALGO
     printf "Binding Energies" > $2/vs_energymatrix.csv
     
+    # x axis
     while read receptor_byline
     do
         printf "," >> $2/vs_energymatrix.csv
@@ -141,6 +187,11 @@ function vs_genenergymatrix ()
 
 function vs_summaries ()
 {
+    # 1- receptor name, 
+    # 2- lig name, 
+    # 3- working dir, 
+    # 4- MGL_dir, 
+    # 5- result_dir
     cd $3/docking/$1/$2/
     $4/bin/pythonsh $4/MGLToolsPckgs/AutoDockTools/Utilities24/summarize_results4.py -d $3/docking/$1/$2/ -b -k -r $1.pdbqt -o summary > /dev/null 2>&1
     awk 'BEGIN {FS=","; OFS=","}; NR==2 {print "'$1'","'$2'",$5,$7}' summary | tr -d [:space:] >> $5/summary_virtualscreening.csv
@@ -149,6 +200,9 @@ function vs_summaries ()
 
 function extension_present ()
 {
+# 1- suffix name
+# 2- directory
+# return true or false
     cd $2
     local filecount=$(find . -maxdepth 1 -type f -name "*.$1" | wc -l)
     if [[ $filecount -ge 1 ]]
@@ -163,12 +217,47 @@ function print_err_color ()
 {
     printf "\n\n\e[1m\e[91mERROR: \e[39m$1\e[0m\n\n"
 }
+
+function gen_best_structure ()
+{
+# 1- protein name
+# 2- ligand name
+# 3- working_dir
+# 4- result_dir
+    local dlg_file="$3/docking/$1/$2/$1__$2.dlg"
+    mkdir -p $4/best_structures/$1/$2/
+    cat $3/docking/$1/$2/$1.pdbqt | awk '/^(ATOM|TER)/ {print}' > $4/best_structures/$1/$2/DOCKED_COMPLEX.pdb
+    trigger=0
+    while read line
+    do
+        if [[ "$line" =~ ^MODEL ]]
+        then
+            trigger=$(( trigger+1 ))
+        fi
+        if [ "$trigger" == 1 ]
+        then
+            if [[ "$line" =~ ^ATOM ]] || [[ "$line" =~ ^TER ]]
+            then
+                echo "$line" >> $4/best_structures/$1/$2/DOCKED_LIGAND.pdb
+                echo "$line" >> $4/best_structures/$1/$2/DOCKED_COMPLEX.pdb
+            fi
+        fi
+    done < ${dlg_file}
+}
+# endregion functions
+
+# region preparations 
+
+    # region for CLI
 export prepare_receptor
 export AD_ALGO
 export AUTO_SITE
 export -f extension_present
 export ligand_dir
 export ALGO
+    # endregion for CLI
+
+# create list of proteins and ligands in csv
 
 bash $SCRIPTPATH/Modules/vs_printCLI.sh -c 6
 
@@ -188,6 +277,7 @@ then
 fi
 
 echo "Virtual Screening Preparation: Allocating input files"
+# read csv, the allocate files *working_dir/*protein/*ligand/*files*
 cat ${temp_file} | parallel -j 0 mkdir -p $working_dir/docking/{1}/{2} :::: $working_dir/proteins.csv :::: $working_dir/ligands.csv
 cat ${temp_file} | parallel -j 0 cp -a $protein_dir/{1}{3} $working_dir/docking/{1}/{2} :::: $working_dir/proteins.csv :::: $working_dir/ligands.csv ::: $prot_ext
 cat ${temp_file} | parallel -j 0 cp -a $ligand_dir/{2}.pdbqt $working_dir/docking/{1}/{2} :::: $working_dir/proteins.csv :::: $working_dir/ligands.csv
@@ -219,8 +309,13 @@ then
     parallel -j $number_jobs --eta prepareDPF "$working_dir/docking/{1}/{2}" "{1}" "{2}" "$MGL_dir" :::: $working_dir/proteins.csv :::: $working_dir/ligands.csv
 fi
 
+# endregion
+
+# region virtual screening
+
 if [[ $ALGO == "AD" ]]
 then
+    # Run autodock4
     bash $SCRIPTPATH/Modules/vs_printCLI.sh -c 7
     echo "Virtual Screening: grid maps processing... "
     export -f autogrid
@@ -233,6 +328,7 @@ then
 
 elif [[ $ALGO == "VINA" ]]
 then
+    # Run vina
     bash $SCRIPTPATH/Modules/vs_printCLI.sh -c 8
     echo "Virtual Screening: virtual screening... "
     export -f ad_vina
@@ -240,6 +336,9 @@ then
 
 fi
 
+# endregion
+
+# region analysis 
 bash $SCRIPTPATH/Modules/vs_printCLI.sh -c 9
 if [[ $ALGO == "AD" ]]
 then
@@ -256,9 +355,17 @@ then
 fi
 vs_genenergymatrix "$working_dir" "$result_dir" "$ALGO"
 
+if [[ $ALGO == "AD" ]]
+then
+    export -f gen_best_structure
+    cat ${temp_file} | parallel -j 0 gen_best_structure "{1}" "{2}" "$working_dir" "$result_dir" :::: $working_dir/proteins.csv :::: $working_dir/ligands.csv
+fi
+
 bash $SCRIPTPATH/Modules/vs_printCLI.sh -c 10
 printf "DONE! Check the RESULT folder in your working directory!\n\n"
+# endregion
 
+# region unset functions 
 unset -f check_exist
 unset -f vina_check_cpu
 unset -f prepareGPF
@@ -274,3 +381,4 @@ unset AUTO_SITE
 unset -f extension_present
 unset ligand_dir
 unset ALGO
+# endregion
